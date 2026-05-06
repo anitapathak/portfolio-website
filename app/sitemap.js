650d9864611950927ee032e5636afc24dcf1f2d5
Anita Pathak <anita.pathak2062@gmail.com>
2026-05-06 20:03:04 +0545

    Add sitemap and verify domain

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://anitapathak.com.np',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ];
+}
